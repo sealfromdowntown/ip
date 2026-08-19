b116466bdc0aa12f9fb5b3bce0db78f7c4f6da70
sealfromdowntown <junhong.lim960@gmail.com>
2026-08-19 15:54:21 +0800

    Echo commands from user

--- src/main/java/Chim.java
+++ src/main/java/Chim.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chim {
     public static void main(String[] args) {
         String logo =
@@ -17,7 +19,22 @@ public class Chim {
         System.out.println("What can I do for you?");
         System.out.println(line);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine().trim();
+
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
