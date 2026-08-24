fe75cabc8c184b205b1638d706946a13f717fd70
sealfromdowntown <junhong.lim960@gmail.com>
2026-08-24 22:50:57 +0800

    Add toFileFormat method to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -29,6 +29,33 @@ public class Task {
         return type;
     }
 
+    /**
+     * Returns this task formatted as a single line for saving to disk.
+     * Subclasses append any extra fields they need (e.g. by/from/to).
+     *
+     * @return Pipe-delimited representation of this task.
+     */
+    public String toFileFormat() {
+        return getTypeSymbol() + " | " + getStatusDigit() + " | " + description;
+    }
+
+    protected String getStatusDigit() {
+        return isDone ? "1" : "0";
+    }
+
+    protected String getTypeSymbol() {
+        switch (type) {
+            case TODO:
+                return "T";
+            case DEADLINE:
+                return "D";
+            case EVENT:
+                return "E";
+            default:
+                return "?";
+        }
+    }
+
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
