d25d29815c7b96c4a1de26516281221ffcc7280a
sealfromdowntown <junhong.lim960@gmail.com>
2026-09-02 02:04:44 +0800

    Fix lexicographical order for Scene import

--- src/main/java/chim/gui/Main.java
+++ src/main/java/chim/gui/Main.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import chim.Chim;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.image.Image;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
