f23bf9dadcc7646d8b453a4f4618150864273d9e
sealfromdowntown <junhong.lim960@gmail.com>
2026-09-02 01:52:04 +0800

    Add background image with translucent scroll pane

--- src/main/java/chim/gui/Main.java
+++ src/main/java/chim/gui/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getStylesheets().add(Main.class.getResource("/css/main.css").toExternalForm());
             stage.setScene(scene);
             stage.setTitle("Chim");
             stage.setMinHeight(220);
